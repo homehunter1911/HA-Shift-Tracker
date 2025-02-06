7fb471b419f53bf948584a2459243c55d2f19032
homehunter1911 <537ers@gmail.com>
2025-02-06 15:03:08 +0100

    Update config_flow.py

--- custom_components/shift_tracker/config_flow.py
+++ custom_components/shift_tracker/config_flow.py
@@ -1,10 +1,15 @@
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 class ShiftTrackerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handles initial setup flow for HA Shift Tracker."""
+    """Handle the initial config flow."""
 
     async def async_step_user(self, user_input=None):
         """Skip configuration at setup and just create an entry."""
         return self.async_create_entry(title="HA Shift Tracker", data={})
+
+    @staticmethod
+    def async_get_options_flow(entry):
+        """Return the options flow handler for Home Assistant."""
+        from .options_flow import ShiftTrackerOptionsFlowHandler
+        return ShiftTrackerOptionsFlowHandler(entry)
